55151151471f95206ad0ba3f88d77b6895b8a08c
ashtelmah <ashtelmah@gmail.com>
2019-03-15 11:42:33 +0200

    Это наш первый комит

---
+++ Lesson1/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var obj = {};
+let persone = {
+       name: 'John',
+       age: 25,
+       isMarried: false
+
+};
+console.log(persone["name"]);
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[0]);
+
+// let answer=confirm("Are you here?");
+// console.log(answer);
+
+//  let answerp=+prompt("How old are you?", "18");
+//  console.log(typeof(answerp));
+
+//console.log(4+ " - obj");
+
+let incr = 10,
+       decr = 10;
+
+console.log(2 === "2");
+
+let isChecked = false,
+       isClose = false;
+
+       console.log(isChecked || !isClose);
